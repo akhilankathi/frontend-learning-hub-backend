4ffb8f759fc7933a5a27e4a5f3582cd7bc6cef2c
Akhil Ankathi <91078213+akhilankathi@users.noreply.github.com>
2026-02-09 13:35:45 +0000

    add tsconfig changes

--- src/main.ts
+++ src/main.ts
@@ -1,6 +1,9 @@
+import 'tsconfig-paths/register';
 import { NestFactory } from '@nestjs/core';
 import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
